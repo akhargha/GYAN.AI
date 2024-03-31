56c06556cf6f5d3cbaec7a104cd79b057f131df6
Shivanshu <shivanshu.dwivedi@trincoll.edu>
2024-03-31 04:48:36 -0400

    Anupam's Amends

--- StrategizerApp.py
+++ StrategizerApp.py
@@ -1,8 +1,15 @@
 from flask import Flask, jsonify
 import json
 from openai import OpenAI
+import requests
+import json
+from flask_cors import CORS  # Import CORS
+import os
 
 app = Flask(__name__)
+
+CORS(app)
+
 client = OpenAI(api_key="") # GPT Key
 
 def generate_study_plan(prompt_notes):
@@ -13,6 +20,7 @@ def generate_study_plan(prompt_notes):
         max_tokens=1024
     )
     notes = response.choices[0].text
+    print("TESTING 2" + notes)
     return notes
 
 @app.route('/generate-study-plan', methods=['GET'])
@@ -27,16 +35,17 @@ def get_study_plan():
         # Generate the study plan
         # Prepare the prompt for the GPT model based on the topic
         prompt_text = f"{topic} Generate a well-structured brief study plan with time-line for the given topic."
-        study_plan = generate_study_plan(topic)
+        print(prompt_text + "TESTING")
+        study_plan = generate_study_plan(prompt_text)
 
 
         # Save the study plan to strategy.json
-        with open('uploads/strategy.json', 'w') as outfile:
-            json.dump({"study_plan": study_plan}, outfile, indent=4)
+        with open(os.path.join('uploads', 'strategy.json'), 'w') as f:
+            json.dump(study_plan, f, indent=4)
 
         return jsonify({"study_plan": study_plan})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5007)
